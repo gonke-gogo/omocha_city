c72a1bed60553277202ffa8e4da9df12d600f3d7
kengoyoshida <hukugonke@gmail.com>
2023-01-10 06:48:03 +0900

    create user_session controller,view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'top_pages#top'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#delete'
   get 'categories/index'
   get 'categories/edit'
   get 'favorites/create'
   get 'favorites/destroy'
+  
   resources :comments, only: %i[create destroy]
   resources :posts
   resources :users, only: %i[new create]
