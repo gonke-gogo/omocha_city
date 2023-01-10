5c583fcf5cf9b0edf286d7d6d5035c7c22ab4f93
kengoyoshida <hukugonke@gmail.com>
2023-01-11 06:06:25 +0900

    user registration and login function, header function

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root to: 'top_pages#top'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#delete'
+  get 'logout', to: 'user_sessions#destroy'
   get 'categories/index'
   get 'categories/edit'
   get 'favorites/create'
   get 'favorites/destroy'
-  
+
   resources :comments, only: %i[create destroy]
   resources :posts
   resources :users, only: %i[new create]
