67d29fe84d9639c1df82204e407a1c8d6744c864
kengoyoshida <hukugonke@gmail.com>
2023-01-12 20:59:03 +0900

    add: skip_before_action, fixed: typo_destroy layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  before_action :require_login
+
+  private
+  def not_authenticated
+    redirect_to login_path, warning: "ログインしてください"
+  end
 end
