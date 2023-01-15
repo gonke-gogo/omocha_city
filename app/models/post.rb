df1285973c921fbd2811c47eb7e749231f3631bf
kengoyoshida <hukugonke@gmail.com>
2023-01-15 21:53:57 +0900

    add: model association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :toy_image, ToyImageUploader
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :toy_name, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: {maximum: 1000}
