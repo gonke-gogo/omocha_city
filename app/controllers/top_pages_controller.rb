45b0ca20d9596e343b672cca0008af6e09f86cf2
kengoyoshida <hukugonke@gmail.com>
2023-01-29 22:12:05 +0900

    fix: top_pages_controller

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,5 +1,20 @@
 class TopPagesController < ApplicationController
   skip_before_action :require_login
+  before_action :category_all
+
   def top
+    @categories = Category.all
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @posts = @category.posts.includes([:user, :categories]).order(created_at: :desc).page(params[:page])
+    else
+      @posts = Post.all.includes([:user, :categories]).order(created_at: :desc).page(params[:page])
+    end
+  end
+
+  private
+
+  def category_all
+    @category_all = Category.all
   end
 end
