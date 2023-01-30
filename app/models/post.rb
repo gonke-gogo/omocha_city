38b916420df03b2c652e4cad2011183f71870172
kengoyoshida <hukugonke@gmail.com>
2023-01-30 15:31:22 +0900

    create: post_age relationship

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
+  has_many :post_ages, dependent: :destroy
+  has_many :ages, through: :post_ages
 
   validates :toy_name, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: {maximum: 1000}
--- app/models/target_age.rb
+++ app/models/target_age.rb
@@ -1,2 +1,4 @@
 class TargetAge < ApplicationRecord
+  has_many :post_ages
+  has_many :posts, through: :post_ages
 end
