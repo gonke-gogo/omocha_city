105fef5cdd7a5405b0ef8c4e215e258fb0e2a698
kengoyoshida <hukugonke@gmail.com>
2023-01-30 21:49:31 +0900

    add: target_age_posts_index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,12 +7,12 @@ class PostsController < ApplicationController
   def index
     if params[:category_id]
       @category = Category.find(params[:category_id])
-      @posts = @category.posts.includes([:user, :categories]).order(created_at: :desc).page(params[:page])
+      @posts = @category.posts.includes([:user, :categories, :target_ages]).order(created_at: :desc).page(params[:page])
     elsif params[:target_age_id]
       @target_age = TargetAge.find(params[:target_age_id])
       @posts = @target_age.posts.includes([:user, :target_ages]).order(created_at: :desc).page(params[:page])
     else
-      @posts = Post.all.includes([:user, :categories]).order(created_at: :desc).page(params[:page])
+      @posts = Post.all.includes([:user, :categories, :target_ages]).order(created_at: :desc).page(params[:page])
     end
   end
 
