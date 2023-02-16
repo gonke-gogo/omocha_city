1421a5171372acf0dc7d21a7730a1e43628b7c67
kengoyoshida <hukugonke@gmail.com>
2023-02-17 06:37:27 +0900

    entrypoint.shを作成、seedsファイルに楽天系の値を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,9 @@ end
   Post.create(
     user: User.offset(rand(User.count)).first,
     toy_name: "タイトル#{index}",
-    content: "本文#{index}"
+    content: "本文#{index}",
+    netshop_link: "https://item.rakuten.co.jp/netbaby/4904810128465/",
+    rakuten_toyname: "はじめて英語 くまのプーさんと絵本でおしゃべり！ゆびさき知育いっぱいできた(1個)【タカラトミー】[おもちゃ 玩具 知育 ゆびさき プーさん]",
+    rakuten_toyimage: "https://thumbnail.image.rakuten.co.jp/@0_mall/netbaby/cabinet/465/4904810128465.jpg?_ex=128x128">,
   )
 end
