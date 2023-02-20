f99175602c00fd0e91d8e223109aaf95964a3c50
kengoyoshida <hukugonke@gmail.com>
2023-02-20 14:04:28 +0900

    config.assets.css_compressor = nilを追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.css_compressor = nil
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
