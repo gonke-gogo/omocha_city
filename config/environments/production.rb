e222ec224ff3c18b5334ade3c5e8fd84c58d28cf
kengoyoshida <hukugonke@gmail.com>
2023-02-21 21:17:55 +0900

    config.assets.compile = trueの追加2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -77,7 +77,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.assets.compile = true
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
