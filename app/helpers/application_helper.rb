c3ff67088c36e14077fe481229afb9e779271eb0
kengoyoshida <hukugonke@gmail.com>
2023-03-17 13:21:34 +0900

    静的なOGPの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,31 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def default_meta_tags
+    {
+      site: 'おもちゃシティー',
+      title: 'おもちゃの投稿&検索サイト',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'おもちゃの投稿サービスです。我が子の使っているおもちゃの投稿や、気になるおもちゃを検索・購入してみてください！',
+      keywords: 'おもちゃ,',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.jpg'), 
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image', 
+        site: '@', 
+        image: image_url('ogp.jpg')
+      }
+    }
+  end
 end
