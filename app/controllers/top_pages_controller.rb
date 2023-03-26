561ffe5fbb0a3599a234857119f1bc00da9c114c
kengoyoshida <hukugonke@gmail.com>
2023-03-26 16:57:26 +0900

    おもちゃ人気記事3選の抽出

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -3,5 +3,7 @@ class TopPagesController < ApplicationController
   before_action :target_age_all
   before_action :category_all
 
-  def top; end
+  def top
+    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
+  end
 end
