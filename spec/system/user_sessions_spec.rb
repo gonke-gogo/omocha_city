db067cf5e4fac973ed294261718caf36d884b20c
kengoyoshida <hukugonke@gmail.com>
2023-04-14 22:26:30 +0900

    ログインページの表示、ログイン成功のテスト 作成

---
+++ spec/system/user_sessions_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'UserSessions', type: :system do
+  let(:user) { create(:user) }
+
+  describe 'ログインページの表示' do
+    it 'ログインページが正常に表示されること' do
+      visit login_path
+      expect(page).to have_content 'ログイン'
+    end
+  end
+
+  describe 'ログイン処理' do
+    before do
+      visit login_path
+    end
+
+    context '正しいログイン情報が渡された場合' do
+      it 'ユーザーがログインし、投稿一覧ページに遷移する' do
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect(page).to have_content 'ログインしました'
+        expect(current_path).to eq posts_path
+      end
+    end
+
+  end
+
+end
