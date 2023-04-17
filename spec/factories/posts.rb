a5a4c8a509bed44fc38759bde184f33a03b0e656
kengoyoshida <hukugonke@gmail.com>
2023-04-18 06:22:15 +0900

    詳細画面での類似投稿のランダム表示のテスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :post do
     sequence(:toy_name) { |n| "Test toy name #{n}" }
     content { "test content" }
+    toy_image {File.open("#{Rails.root}/app/assets/images/default.jpg")}
     association :user
   end
 end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe 'Posts', type: :system do
   before do
     @user = FactoryBot.create(:user)
     @post = FactoryBot.create(:post, user: @user)
+    @category = FactoryBot.create(:category)
+    @target_age = FactoryBot.create(:target_age)
+    @post.categories << @category
+    @post.target_ages << @target_age
+
+    @related_post = FactoryBot.create(:post, user: @user)
+    @related_post.categories << @category
+    @related_post.target_ages << @target_age
     login_as(@user)
   end
 
@@ -12,15 +20,21 @@ RSpec.describe 'Posts', type: :system do
       visit posts_path
     end
 
-    fit '一覧画面におもちゃが正しく表示される' do
+    it '一覧画面におもちゃが正しく表示される' do
       expect(page).to have_content(@post.toy_name)
       expect(page).to have_content(@post.content)
     end
     
-    fit '一覧ページから詳細ページへの移動' do
+    it '一覧ページから詳細ページへの移動' do
       click_on @post.toy_name
       expect(page).to have_content(@post.toy_name)
       expect(page).to have_content(@post.content)
+    end
+
+    fit '詳細画面にて、ランダムに類似したおもちゃが表示されること' do
+      visit post_path(@post)
+      expect(page).to have_content(@related_post.toy_name)
+      expect(page).to have_selector("img[src$='#{@related_post.toy_image.url}']")
     end
   end
 end
